194e3b9e52854b1c092acbd7bf363021a5848816
Dell <Dell@Dell-PC>
2016-03-02 22:33:07 +0200

    11

--- firstassignment/src/eg/edu/alexu/csd/datastructure/hangman/cs31/Hangman_game.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/hangman/cs31/Hangman_game.java
@@ -7,10 +7,8 @@ import java.util.Random;
 public class Hangman_game implements IHangman {
 	static String[] temp;
 	static String str = new String();
-	static Character guess;
 	static int counter=0, maximum;
 	static String out = null;
-	static boolean[] global=new boolean [20];
 	 static Character[] show =new Character[20];
 
 
@@ -25,12 +23,13 @@ public class Hangman_game implements IHangman {
 
 	@Override
 	public String selectRandomSecretWord() {
-		str=null;
+		
 		 Random rand = new Random();
 			int num = rand.nextInt(temp.length);
 			str = temp[num];
 		for(int i=0;i<str.length();i++)
 				show[i]='-';
+		
 		return str;
 	}
 
@@ -39,18 +38,10 @@ public class Hangman_game implements IHangman {
 		int flag = 0;
      
 			for (int i = 0; i < str.length(); i++) {
-				if (str.toLowerCase().charAt(i) == Character.toLowerCase(c)&& global[i] == false) {
+				if (str.toLowerCase().charAt(i) == Character.toLowerCase(c)) {
 					show[i] = c;
-
-					global[i] = true;
-					
 					flag = 1;
 				}
-			}
-			if (flag == 1) {
-				
-				for (int i=0;i<str.length();i++)
-					show[i]=c;
 			}
 
 			if (flag == 0) {
@@ -58,7 +49,7 @@ public class Hangman_game implements IHangman {
 				counter++;
 				
 				if (counter >= maximum)
-					out = null;
+					return null;
 
 			}
 			out = String.valueOf(show);
@@ -76,31 +67,5 @@ public class Hangman_game implements IHangman {
 
 	}
 
-	/*public static void main(String[] args) {
-		
-		IHangman hangman = new Hangman_game(); // Here you will create an object
-												// of your class
-		hangman.setDictionary(temp);
-		hangman.setMaxWrongGuesses(5);
-		String secret = hangman.selectRandomSecretWord();
-		while (secret==null)
-			secret = hangman.selectRandomSecretWord();
-		Scanner input = new Scanner(System.in); // Get user input
-        //System.out.println("enter"); 
-       guess =input.next().toUpperCase().charAt(0);
-		do {
-			String result = hangman.guess(guess);
-			if (result == null) {
-				System.out.println("Fail! correct answer = '" + secret + "'"); // fail
-				return;
-			}
-			System.out.println(result);
-			if (!result.contains("-")) {
-				System.out.println("Well Done!"); // win
-				return;
-			}
-			guess = input.next().toUpperCase().charAt(0);// scanning a character
-		} while (true);
-	}*/
-
+	
 }
