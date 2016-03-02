0d96ea4d8f117a048f8de460ca774b343a5abe26
Dell <Dell@Dell-PC>
2016-03-02 14:38:08 +0200

    third

--- firstassignment/src/eg/edu/alexu/csd/datastructure/hangman/cs31/Hangman_game.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/hangman/cs31/Hangman_game.java
@@ -1,63 +1,26 @@
 package eg.edu.alexu.csd.datastructure.hangman.cs31;
 
 import eg.edu.alexu.csd.datastructure.hangman.IHangman;
-
-import java.util.Scanner;
-import java.io.FileInputStream;
-import java.io.File;
-import java.io.IOException;
 import java.util.Random;
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.FileNotFoundException;
+
 
 public class Hangman_game implements IHangman {
-	static String[] temp = new String[350000];
+	static String[] temp;
 	static String str = new String();
 	
 	static Character guess;
 	static int counter, maximum;
 	static String out = null;
 
-	static boolean[] global = new boolean[20];
-	 static Character[] show = new Character[20];
+	static boolean[] global;
+	 static Character[] show ;
 
 
 	@Override
 	public void setDictionary(String[] words) {
-
-		File file = new File("C:\\dictionary.txt");
-		FileInputStream fis = null;
-		BufferedInputStream bis = null;
-		DataInputStream dis = null;
-
-		try {
-			fis = new FileInputStream(file);
-
-			// Here BufferedInputStream is added for fast reading.
-			bis = new BufferedInputStream(fis);
-			dis = new DataInputStream(bis);
-
-			// dis.available() returns 0 if the file does not have more lines.
-			while (dis.available() != 0) {
-
-				// this statement reads the line from the file and put it in the
-				// array
-				for (int i = 0; i < 350000; i++)
-					words[i] = dis.readLine();
-			}
-
-			// dispose all the resources after using them.
-			fis.close();
-			bis.close();
-			dis.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		for (int j = 0; j < 350000; j++)
+        temp=new String[words.length];
+		
+		for (int j = 0; j < words.length; j++)
 			temp[j] = words[j];
 	}
 
@@ -72,9 +35,9 @@ public class Hangman_game implements IHangman {
 	@Override
 	public String guess(Character c) {
 		int flag = 0;
-	 //boolean[] check = new boolean[str.length()];
-         
-		//Character[] show = new Character[str.length()];
+       global=new boolean[str.length()];  
+       show= new Character [str.length()];
+	
 		 
 		if (c != null) {
 			for (int i = 0; i < str.length(); i++) {
@@ -140,7 +103,7 @@ public class Hangman_game implements IHangman {
 
 	}
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		IHangman hangman = new Hangman_game(); // Here you will create an object
 												// of your class
 		hangman.setDictionary(temp);
@@ -149,8 +112,8 @@ public class Hangman_game implements IHangman {
 		while (secret==null)
 			secret = hangman.selectRandomSecretWord();
 		Scanner input = new Scanner(System.in); // Get user input
-        System.out.println("enter"); 
-       guess =input.next().charAt(0);
+        //System.out.println("enter"); 
+       guess =null;
 		do {
 			String result = hangman.guess(guess);
 			if (result == null) {
@@ -165,5 +128,5 @@ public class Hangman_game implements IHangman {
 			guess = input.next().charAt(0);// scanning a character
 		} while (true);
 	}
-
+*/
 }
