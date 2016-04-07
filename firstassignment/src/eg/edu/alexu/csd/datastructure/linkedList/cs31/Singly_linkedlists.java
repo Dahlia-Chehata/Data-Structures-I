24a6621c1fa72b64c8c0c9601712fb0225216330
Dell <Dell@Dell-PC>
2016-04-07 23:50:13 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs31/Singly_linkedlists.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs31/Singly_linkedlists.java
@@ -16,7 +16,6 @@ public class Singly_linkedlists implements ILinkedList{
 	public void add(int index, Object element) throws RuntimeException{
 		// TODO Auto-generated method stub
 		SLNode new_node =new SLNode( );
-	
 		SLNode pointer=head;
 		new_node.value=element;
 		
@@ -52,43 +51,76 @@ public class Singly_linkedlists implements ILinkedList{
 	@Override
 	public void add(Object element) {
 		// TODO Auto-generated method stub
-		
+		SLNode pointer=head;
+		SLNode new_node=new SLNode();
+		new_node.value=element;
+		while (pointer !=null)
+			pointer=pointer.next;
+	
+	   pointer.next = new_node;
+	   new_node.next=null;
+	   size++;
 	}
+	   
 
 	@Override
 	public Object get(int index) {
 		// TODO Auto-generated method stub
-		return null;
+		SLNode pointer=head;
+		for (int i=0;i<index;i++)
+			pointer =pointer.next;
+		
+		return pointer.value;
 	}
 
 	@Override
 	public void set(int index, Object element) {
 		// TODO Auto-generated method stub
+		SLNode pointer=head;
+		for (int i=0;i<index;i++)
+			pointer =pointer.next;
+		
+		pointer.value=element;
+		
 		
 	}
 
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+		 head=null;
+		 size=0;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
+		if (head==null &&size==0)
+			return true;
 		return false;
 	}
 
 	@Override
 	public void remove(int index) {
 		// TODO Auto-generated method stub
-		
+		SLNode pointer=head;
+		SLNode temp;
+		for (int i=0;i<index-1;i++){
+			pointer=pointer.next;
+		}
+		temp=pointer.next;
+		pointer.next=temp.next;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		SLNode pointer =head;
+		while (pointer!=null){
+			pointer=pointer.next;
+			size++;
+		}
+		return size;
 	}
 
 	@Override
@@ -100,6 +132,12 @@ public class Singly_linkedlists implements ILinkedList{
 	@Override
 	public boolean contains(Object o) {
 		// TODO Auto-generated method stub
+		SLNode pointer=head;
+		while (pointer!=null){
+			if (pointer.value==o)
+				return true;
+			pointer=pointer.next;
+		}
 		return false;
 	}
 
