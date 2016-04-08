30da333fbfbf055f0923ef53e6e66c0a853daf31
Dell <Dell@Dell-PC>
2016-04-08 11:50:23 +0200

    7

---
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs31/DNode.java
@@ -0,0 +1,19 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs31;
+
+public class DNode {
+    public Object value;
+    public DNode next;
+    public DNode prev;
+
+ 
+  public DNode (){
+	  value =null;
+	  next =null;
+	  prev=null;
+  }
+  public DNode (DNode p,Object o,DNode n){
+	  prev =p;
+	  value=o;
+	  next=n;
+  }
+}
