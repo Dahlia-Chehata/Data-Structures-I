139e0a2a484991b29bcc34b1b8fb7080b11fb728
Dell <Dell@Dell-PC>
2016-04-14 12:32:26 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs31/Polynomial_Solver.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs31/Polynomial_Solver.java
@@ -567,7 +567,8 @@ public class Polynomial_Solver implements IPolynomialSolver {
 			System.out.println("Please choose an action");
 			System.out.println("-----------------------");
 			System.out.println("1- Set a polynomial variable");
-			System.out.println("2- Print the value of a polynomial variable");
+			System.out.println
+			("2- Print the value of a polynomial variable");
 			System.out.println("3- Add two polynomials");
 			System.out.println("4- Subtract two polynomials");
 			System.out.println("5- Multiply two polynomials");
