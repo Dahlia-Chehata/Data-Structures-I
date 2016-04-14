c799a8e171382b54855730633c6f94a6c780e2ef
Dell <Dell@Dell-PC>
2016-04-14 11:11:43 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs31/Singly_linkedlists.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/linkedList/cs31/Singly_linkedlists.java
@@ -17,31 +17,31 @@ public class Singly_linkedlists implements ILinkedList {
 	@Override
 	public void add(int index, Object element) throws RuntimeException {
 		// TODO Auto-generated method stub
-		SLNode new_node =new SLNode();
-		SLNode pointer=head;
-		new_node.value=element;
+		SLNode new_node = new SLNode();
+		SLNode pointer = head;
+		new_node.value = element;
 		
 		if (index==0) {
-			new_node.next=head;
-			head=new_node;
+			new_node.next = head;
+			head = new_node;
 			size++;
 		}
 		
-		else if (index==size) {
+		else if (index == size) {
 	    while (pointer.next!=null)
-	    	pointer=pointer.next;
+	    	pointer = pointer.next;
 			 pointer.next = new_node;
-			 new_node.next=null;
+			 new_node.next = null;
 			size++;
 
 		}
 		else  if (index> 0 && index < size) {
 		   for (int i=0;i<index-1;i++){
-			   pointer=pointer.next;
+			   pointer = pointer.next;
 		   }
 			
-			new_node.next=pointer.next;
-		    pointer.next=new_node;
+			new_node.next = pointer.next;
+		    pointer.next = new_node;
 		    
 		    size++;
 	   }
@@ -53,19 +53,19 @@ public class Singly_linkedlists implements ILinkedList {
 	@Override
 	public void add(Object element) {
 		// TODO Auto-generated method stub
-		SLNode pointer=head;
-		SLNode new_node=new SLNode();
-		new_node.value=element;
-		if (head==null){
+		SLNode pointer = head;
+		SLNode new_node = new SLNode();
+		new_node.value = element;
+		if (head == null) {
 			//new_node.next=null;
-			head=new_node;
+			head = new_node;
 		}
 		else{
 		while (pointer.next !=null)
-			pointer=pointer.next;
+			pointer = pointer.next;
 	
 	   pointer.next = new_node;
-	   new_node.next=null;
+	   new_node.next = null;
 	   }
 	   size++;
 	}
@@ -74,10 +74,10 @@ public class Singly_linkedlists implements ILinkedList {
 	@Override
 	public Object get(int index) throws RuntimeException {
 		// TODO Auto-generated method stub
-		SLNode pointer=head;
-		if (index>=0&&index<size){
+		SLNode pointer = head;
+		if (index>=0 && index<size) {
 		for (int i=0;i<index;i++)
-			pointer =pointer.next;
+			pointer = pointer.next;
 		
 		return pointer.value;
 		}
@@ -88,12 +88,12 @@ public class Singly_linkedlists implements ILinkedList {
 	@Override
 	public void set(int index, Object element) throws RuntimeException {
 		// TODO Auto-generated method stub
-		SLNode pointer=head;
-		 if (index>=0&&index<size){
+		SLNode pointer = head;
+		 if (index>=0 && index<size){
 		for (int i=0;i<index;i++)
-			pointer =pointer.next;
+			pointer = pointer.next;
 		
-		pointer.value=element;
+		pointer.value = element;
 		 }
 		 else 
 			 throw new RuntimeException();
@@ -103,14 +103,14 @@ public class Singly_linkedlists implements ILinkedList {
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		 head=null;
-		 size=0;
+		 head = null;
+		 size = 0;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		if (head==null)
+		if (head == null)
 			return true;
 		return false;
 	}
@@ -118,25 +118,25 @@ public class Singly_linkedlists implements ILinkedList {
 	@Override
 	public void remove(int index) throws RuntimeException {
 		// TODO Auto-generated method stub
-		SLNode pointer=head;
+		SLNode pointer = head;
 		SLNode temp;
 		if(isEmpty())
 			throw  new RuntimeException();
-		else if(index==0){
-			temp=head;
-			head=head.next;
-			temp.next=null;
+		else if(index == 0){
+			temp = head;
+			head = head.next;
+			temp.next = null;
 			size--;
 		}
 		
 			
-		else if (index>0&&index<size) {
+		else if (index>0 && index<size) {
 		for (int i=0;i<index-1;i++){
-			pointer=pointer.next;
+			pointer = pointer.next;
 		}
-		temp=pointer.next;
-		pointer.next=temp.next;
-		temp.next=null;
+		temp = pointer.next;
+		pointer.next = temp.next;
+		temp.next = null;
 		size--;
 		}
 		else 
@@ -154,10 +154,10 @@ public class Singly_linkedlists implements ILinkedList {
 	public ILinkedList sublist(int fromIndex, int toIndex)throws RuntimeException {
 		// TODO Auto-generated method stub
 		ILinkedList new_list = new Singly_linkedlists();
-		SLNode pointer=head;
+		SLNode pointer = head;
 		if (fromIndex>=0 && fromIndex<size && toIndex>=0 && toIndex<size) {
 			for (int i=0;i<fromIndex;i++)
-				pointer=pointer.next;
+				pointer = pointer.next;
 			for (int i=fromIndex;i<=toIndex;i++) {
 				new_list.add(pointer.value);
 				pointer=pointer.next;
@@ -171,13 +171,13 @@ public class Singly_linkedlists implements ILinkedList {
 	@Override
 	public boolean contains(Object o) {
 		// TODO Auto-generated method stub
-		SLNode pointer=head;
+		SLNode pointer = head;
 		if (isEmpty())
 			return false;
-		while (pointer!=null) {
+		while (pointer != null) {
 			if (pointer.value.equals(o))
 				return true;
-			pointer=pointer.next;
+			pointer = pointer.next;
 		}
 		return false;
 	}
