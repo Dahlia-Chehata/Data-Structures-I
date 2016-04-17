da6ea654da93522041db7dc618764e8c7f29fed6
Dell <Dell@Dell-PC>
2016-04-17 15:26:24 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
@@ -2,9 +2,32 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 import eg.edu.alexu.csd.datastructure.stack.IExpressionEvaluator;
 public class ExpressionEvaluator implements IExpressionEvaluator {
 
+	 Stack stack =new Stack();
 	@Override
 	public String infixToPostfix(String expression) {
 		// TODO Auto-generated method stub
+		String str = new String();
+		String operand1 = new String();
+		String operand2 = new String();
+		boolean valid;
+		for (int i=0;i<expression.length();i++){
+			if (!((Character)expression.charAt(i)).equals("(")
+					&&!((Character)expression.charAt(i)).equals("*")
+					&&!((Character)expression.charAt(i)).equals("+")
+					&&!((Character)expression.charAt(i)).equals("-")
+					&&!((Character)expression.charAt(i)).equals("/")
+					&&!((Character)expression.charAt(i)).equals(")")){
+				if (!((Character)expression.charAt(i)).equals("(")
+						&&!((Character)expression.charAt(i+1)).equals("*")
+						&&!((Character)expression.charAt(i+1)).equals("+")
+						&&!((Character)expression.charAt(i+1)).equals("-")
+						&&!((Character)expression.charAt(i+1)).equals("/")
+						&&!((Character)expression.charAt(i+1)).equals(")")){
+					valid =false;
+				}
+			}
+			
+		}
 		return null;
 	}
 
