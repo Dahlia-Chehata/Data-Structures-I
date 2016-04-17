410eb6aa3e784219f3c5c55c590c2a0e3973aeda
Dell <Dell@Dell-PC>
2016-04-17 19:04:57 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
@@ -17,7 +17,7 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 			boolean valid=true;
 			for (int i=0;i<expression.length()-1;i++){
 				
-				/*if (expression.charAt(i)!='('
+				if (expression.charAt(i)!='('
 						&& expression.charAt(i)!='*'
 						&& expression.charAt(i)!= '+'
 						&& expression.charAt(i)!='-'
@@ -45,8 +45,8 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 						throw new RuntimeException("kkkk");
 					}
 				}
-				*/
-				if((expression.charAt(i)!='('
+				
+				/*if((expression.charAt(i)!='('
 						|| expression.charAt(i)=='*'
 						|| expression.charAt(i)== '+'
 						|| expression.charAt(i)=='-'
@@ -60,7 +60,7 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 				      //valid=false;
 					throw new RuntimeException("oooo");
 					
-				}
+				}*/
 				   if (expression.charAt(i+1)==')'
 						&& expression.charAt(i+1)=='('){
 					
