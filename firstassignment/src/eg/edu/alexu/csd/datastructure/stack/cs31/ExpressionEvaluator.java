7fac060dd414bb3d6887766a2432cb631775ce14
Dell <Dell@Dell-PC>
2016-04-17 19:02:36 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
@@ -45,7 +45,7 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 						throw new RuntimeException("kkkk");
 					}
 				}
-				
+				*/
 				if((expression.charAt(i)!='('
 						|| expression.charAt(i)=='*'
 						|| expression.charAt(i)== '+'
@@ -60,7 +60,7 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 				      //valid=false;
 					throw new RuntimeException("oooo");
 					
-				}*/
+				}
 				   if (expression.charAt(i+1)==')'
 						&& expression.charAt(i+1)=='('){
 					
