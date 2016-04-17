2d95a95106044204371c58cc59826313c92047f2
Dell <Dell@Dell-PC>
2016-04-17 14:28:40 +0200

    1

---
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
@@ -0,0 +1,17 @@
+package eg.edu.alexu.csd.datastructure.stack.cs31;
+import eg.edu.alexu.csd.datastructure.stack.IExpressionEvaluator;
+public class ExpressionEvaluator implements IExpressionEvaluator {
+
+	@Override
+	public String infixToPostfix(String expression) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int evaluate(String expression) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
