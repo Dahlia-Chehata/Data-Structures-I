0e6e30a9f55d129c491db0f3528e4c227a99a67c
Dell <Dell@Dell-PC>
2016-04-17 19:53:13 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
@@ -62,7 +62,7 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 					}
 				}
 				
-				/*if((expression.charAt(i)=='('
+				if((expression.charAt(i)=='('
 						|| expression.charAt(i)=='*'
 						|| expression.charAt(i)== '+'
 						|| expression.charAt(i)=='-'
@@ -78,7 +78,7 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 				      //valid=false;
 					throw new RuntimeException("oooo");
 					
-				}*/
+				}
 				   if (expression.charAt(i+1)==')'
 						&& expression.charAt(i+1)=='('){
 					
