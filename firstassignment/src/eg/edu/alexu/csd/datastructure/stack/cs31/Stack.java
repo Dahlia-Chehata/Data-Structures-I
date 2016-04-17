87807ffe79fe4e74563769eb19e6116a3aec1a42
Dell <Dell@Dell-PC>
2016-04-17 14:25:07 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/Stack.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/Stack.java
@@ -1,36 +1,38 @@
 package eg.edu.alexu.csd.datastructure.stack.cs31;
+
 import eg.edu.alexu.csd.datastructure.stack.IStack;
 //import eg.edu.alexu.csd.datastructure.linkedList.cs31.SLNode;
 import eg.edu.alexu.csd.datastructure.linkedList.cs31.Singly_linkedlists;
 import eg.edu.alexu.csd.datastructure.linkedList.ILinkedList;
+
 public class Stack implements IStack {
-	
+
 	ILinkedList mylist = new Singly_linkedlists();
 
 	@Override
-	public void add(int index, Object element)throws RuntimeException {
+	public void add(int index, Object element) throws RuntimeException {
 		// TODO Auto-generated method stub
-		 mylist.add(mylist.size()-index, element);
+		mylist.add(mylist.size() - index, element);
 	}
 
 	@Override
-	public Object pop()throws RuntimeException{
+	public Object pop() throws RuntimeException {
 		// TODO Auto-generated method stub
-		Object pop=mylist.get(0);
+		Object pop = mylist.get(0);
 		mylist.remove(0);
 		return pop;
 	}
 
 	@Override
-	public Object peek() {
+	public Object peek() throws RuntimeException {
 		// TODO Auto-generated method stub
-		return mylist.get(0) ;
+		return mylist.get(0);
 	}
 
 	@Override
-	public void push(Object element) {
+	public void push(Object element) throws RuntimeException {
 		// TODO Auto-generated method stub
-		mylist.add(0,element);
+		mylist.add(0, element);
 	}
 
 	@Override
