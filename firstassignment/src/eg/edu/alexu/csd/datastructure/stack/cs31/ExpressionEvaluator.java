a86384b403378fd4f4ec996df7fd9feb830257e9
Dell <Dell@Dell-PC>
2016-04-28 11:24:51 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/stack/cs31/ExpressionEvaluator.java
@@ -1,212 +1,188 @@
 package eg.edu.alexu.csd.datastructure.stack.cs31;
-	import eg.edu.alexu.csd.datastructure.stack.IExpressionEvaluator;
-	import eg.edu.alexu.csd.datastructure.stack.IStack;
-	public class ExpressionEvaluator implements IExpressionEvaluator {
-	
-		 static IStack stack =new Stack();
-		@Override
-		public String infixToPostfix(String expression) throws RuntimeException {
-			
-			// TODO Auto-generated method stub
-		String str = new String();
-			boolean flag=true;
-			if (expression==null){
-			   throw new RuntimeException("bbb");
-				
-			}
-			if (expression.charAt(0)=='/'
-        		||expression.charAt(0)=='*'
-        		||expression.charAt(0)=='+'
-        		||expression.charAt(0)=='-')
-				throw new RuntimeException("starting with operator");
-			
-        /*  for (int i=0;i<expression.length();i++){
-        	  
-				if (expression.charAt(i)!='('
-						&& expression.charAt(i)!=' '
-						&& expression.charAt(i)!='*'
-						&& expression.charAt(i)!= '+'
-						&& expression.charAt(i)!='-'
-						&& expression.charAt(i)!='/'
-					    && expression.charAt(i)!=')'		
-						&&('0'>expression.charAt(i)
-						||'9'<(expression.charAt(i)))){
-					
-			    
-			      throw new RuntimeException("aaaa");
-			    
-				}
-				
-          }*/
-			  
-          for (int i=0;i<expression.length()-1&&flag;i++){	
-		  flag=false;
-		         if ( expression.charAt(i)!='*'
-					&& expression.charAt(i)!= '+'
-					&& expression.charAt(i)!='-'
-					&& expression.charAt(i)!='/')
-	     flag=true;
-          }
-	if (flag) {
-		 throw new RuntimeException("no operator");
-	}
-	for (int i=0;i<expression.length()-1;i++){
-		
-		//test all symbols
-		if(( ('A'<expression.charAt(i)&&expression.charAt(i)<'Z')
-				
-				&&'0'<expression.charAt(i+1)&&expression.charAt(i+1)<'9' )
-				
-				||(('a'<expression.charAt(i)&&expression.charAt(i)<'z')
-				
-				&&  '0'<expression.charAt(i+1)&&expression.charAt(i+1)<'9')){
-				
-			
-			throw new RuntimeException("oooo");
-			
-		}
-	}
-			
-          
-			for (int i=0;i<expression.length()-1;i++){
-				
-				//test all symbols
-				if((expression.charAt(i)=='('
-						|| expression.charAt(i)=='*'
-						|| expression.charAt(i)== '+'
-						|| expression.charAt(i)=='-'
-						|| expression.charAt(i)=='/')
-						&& ( expression.charAt(i+1)=='*'
-						|| expression.charAt(i+1)== '+'
-						|| expression.charAt(i+1)=='-'
-						|| expression.charAt(i+1)=='/'
-					    || expression.charAt(i+1)==')')){
-					
-					throw new RuntimeException("oooo");
-					
-				}
-				   if (expression.charAt(i+1)==')'
-						&& expression.charAt(i+1)=='('){
-					
-					      throw new RuntimeException("ssss");
-				}	
-		}		
-			
 
-			
-				for (int i=0;i<expression.length();i++){
-					if(expression.charAt(i)==' ')
-						continue;
-					if (expression.charAt(i)=='('){
-						stack.push(expression.charAt(i));
-					}else if (expression.charAt(i)==')'){
-						
-						while (stack.size()!=0 &&(char)stack.peek()!='('){
-							str+=stack.pop();
-							str+=" ";
-						}	
-						if (stack.isEmpty()||(!stack.isEmpty()&&(char)stack.peek()!='('))
-							throw new RuntimeException("there is no (");
-						stack.pop();
-					}else if(expression.charAt(i)=='*'||expression.charAt(i)=='/'){
-					    if(stack.isEmpty()){
-					    	stack.push(expression.charAt(i));
-					    }
-						else if ((char)stack.peek()=='+'||(char)stack.peek()=='-'){
-							stack.push(expression.charAt(i));
-						}
-						else {
-							while(stack.size()!=0
-									&&((char)stack.peek()=='*'
-									||(char)stack.peek()=='/')){
-								str+=stack.pop();
-								str+=" ";
-							}
-							stack.push(expression.charAt(i));
-						}
-					}else if(expression.charAt(i)=='+'||expression.charAt(i)=='-'){
-						while (stack.size()!=0 
-								&&((char)stack.peek()=='*'
-								||(char)stack.peek()=='/'
-								||(char)stack.peek()=='+'
-								||(char)stack.peek()=='-')){
-							str+=stack.pop();
-							str+=" ";
-						}
-						stack.push(expression.charAt(i));
+import eg.edu.alexu.csd.datastructure.stack.IExpressionEvaluator;
+import eg.edu.alexu.csd.datastructure.stack.IStack;
+
+public class ExpressionEvaluator implements IExpressionEvaluator {
+
+	static IStack stack = new Stack();
+
+	@Override
+	public String infixToPostfix(String expression) throws RuntimeException {
+
+		// TODO Auto-generated method stub
+		StringBuffer str = new StringBuffer();
+		boolean flag = true;
+		if (expression == null) {
+			throw new RuntimeException("bbb");
+
+		}
+		if (expression.charAt(0) == '/' || expression.charAt(0) == '*' || expression.charAt(0) == '+'
+				|| expression.charAt(0) == '-')
+			throw new RuntimeException("starting with operator");
+
+		/*
+		 * for (int i=0;i<expression.length();i++){
+		 * 
+		 * if (expression.charAt(i)!='(' && expression.charAt(i)!=' ' &&
+		 * expression.charAt(i)!='*' && expression.charAt(i)!= '+' &&
+		 * expression.charAt(i)!='-' && expression.charAt(i)!='/' &&
+		 * expression.charAt(i)!=')' &&('0'>expression.charAt(i)
+		 * ||'9'<(expression.charAt(i)))){
+		 * 
+		 * 
+		 * throw new RuntimeException("aaaa");
+		 * 
+		 * }
+		 * 
+		 * }
+		 */
+
+		for (int i = 0; i < expression.length() - 1 && flag; i++) {
+			flag = false;
+			if (expression.charAt(i) != '*' && expression.charAt(i) != '+' && expression.charAt(i) != '-'
+					&& expression.charAt(i) != '/')
+				flag = true;
+		}
+		if (flag) {
+			throw new RuntimeException("no operator");
+		}
+		for (int i = 0; i < expression.length() - 1; i++) {
+
+			// test all symbols
+			if ((('A' < expression.charAt(i) && expression.charAt(i) < 'Z')
+
+					&& '0' < expression.charAt(i + 1) && expression.charAt(i + 1) < '9')
+
+					|| (('a' < expression.charAt(i) && expression.charAt(i) < 'z')
+
+							&& '0' < expression.charAt(i + 1) && expression.charAt(i + 1) < '9')) {
+
+				throw new RuntimeException("oooo");
+
+			}
+		}
+
+		for (int i = 0; i < expression.length() - 1; i++) {
+
+			// test all symbols
+			if ((expression.charAt(i) == '(' || expression.charAt(i) == '*' || expression.charAt(i) == '+'
+					|| expression.charAt(i) == '-' || expression.charAt(i) == '/')
+					&& (expression.charAt(i + 1) == '*' || expression.charAt(i + 1) == '+'
+							|| expression.charAt(i + 1) == '-' || expression.charAt(i + 1) == '/'
+							|| expression.charAt(i + 1) == ')')) {
+
+				throw new RuntimeException("oooo");
+
+			}
+			if (expression.charAt(i + 1) == ')' && expression.charAt(i + 1) == '(') {
+
+				throw new RuntimeException("ssss");
+			}
+		}
+
+		for (int i = 0; i < expression.length(); i++) {
+			if (expression.charAt(i) == ' ')
+				continue;
+			if (expression.charAt(i) == '(') {
+				stack.push(expression.charAt(i));
+			} else if (expression.charAt(i) == ')') {
+
+				while (stack.size() != 0 && (char) stack.peek() != '(') {
+					str.append(stack.pop());
+					str.append(" ");
+				}
+				if (stack.isEmpty() || (!stack.isEmpty() && (char) stack.peek() != '('))
+					throw new RuntimeException("there is no (");
+				stack.pop();
+			} else if (expression.charAt(i) == '*' || expression.charAt(i) == '/') {
+				if (stack.isEmpty()) {
+					stack.push(expression.charAt(i));
+				} else if ((char) stack.peek() == '+' || (char) stack.peek() == '-') {
+					stack.push(expression.charAt(i));
+				} else {
+					while (stack.size() != 0 && ((char) stack.peek() == '*' || (char) stack.peek() == '/')) {
+						str.append(stack.pop());
+						str.append(" ");
 					}
-					else {
-						if (('A'<=expression.charAt(i)&&expression.charAt(i)<='Z')
-						||('a'<=expression.charAt(i)&&expression.charAt(i)<='z')
-						||('0'<=expression.charAt(i)&&expression.charAt(i)<='9')){
-							while(
-									
-						('A'<=expression.charAt(i)&&expression.charAt(i)<='Z')
-						||('a'<=expression.charAt(i)&&expression.charAt(i)<='z')
-						||('0'<=expression.charAt(i)&&expression.charAt(i)<='9')){
-								
-								str+=expression.charAt(i);
-								i++;
-								
-							}
+					stack.push(expression.charAt(i));
+				}
+			} else if (expression.charAt(i) == '+' || expression.charAt(i) == '-') {
+				while (stack.size() != 0 && ((char) stack.peek() == '*' || (char) stack.peek() == '/'
+						|| (char) stack.peek() == '+' || (char) stack.peek() == '-')) {
+					str.append(stack.pop());
+					str.append(" ");
+				}
+				stack.push(expression.charAt(i));
+			} else if (('A' <= expression.charAt(i) && expression.charAt(i) <= 'Z')
+							|| ('a' <= expression.charAt(i) && expression.charAt(i) <= 'z')
+							|| ('0' <= expression.charAt(i) && expression.charAt(i) <= '9')) {{
+
+				
+					
+
+						while (i<expression.length()
+								&&(('A' <= expression.charAt(i) && expression.charAt(i) <= 'Z')
+								|| ('a' <= expression.charAt(i) && expression.charAt(i) <= 'z')
+								|| ('0' <= expression.charAt(i) && expression.charAt(i) <= '9'))) {
+
+							str.append(expression.charAt(i));
+
+							i++;
+
 						}
-						if(i!=0)
+						// if(i!=0)
 						i--;
-						str+=" ";
-						
+					str.append(" ");
+
 					}
-					
+
 				
-				}
-				
-				while (!stack.isEmpty()){
-					  if ((char)stack.peek()=='(')
-						  throw new RuntimeException("'(' without a ')'");
-			
-				
-					  str+=stack.pop();
-						str+=" ";
-				}
-		      
-		        	
-			return str.toString().trim();//to remove space from end and start of a string
+
+			}
+
 		}
 
-		
-		@Override
-		public int evaluate(String expression) {
-			// TODO Auto-generated method stub
-			
-		   double operand1=0;
-		   double operand2=0;
-		   
-			if (expression==null){
-				 throw new RuntimeException();
-			}
-			//expression =infixToPostfix(expression);
-			
-			for (int i=0;i<expression.length();i++){
-				if (('a'<=(expression.charAt(i))
-						&&'z'>=(expression.charAt(i)))||('A'<=(expression.charAt(i))
-						&&'Z'>=(expression.charAt(i)))){
-								
-					
-			    
-			     throw new RuntimeException("lolololol");
-			}
-	
+		while (!stack.isEmpty()) {
+			if ((char) stack.peek() == '(')
+				throw new RuntimeException("'(' without a ')'");
+
+			str.append(stack.pop());
+			str.append(" ");
 		}
-			
-			for (int i=0;i<expression.length();i++){
-				if (expression.charAt(i) ==' ')
-					continue;
-			if (expression.charAt(i) != '(' 
-					//&& expression.charAt(i) != ' ' 
-					&& expression.charAt(i) != '*'
-					&& expression.charAt(i) != '+' 
-					&& expression.charAt(i) != '-' 
-					&& expression.charAt(i) != '/'
-					&& expression.charAt(i) != ')') {
+
+		return str.toString().trim();// to remove space from end and start of a
+										// string
+	}
+
+	@Override
+	public int evaluate(String expression) {
+		// TODO Auto-generated method stub
+
+		double operand1 = 0;
+		double operand2 = 0;
+
+		if (expression == null) {
+			throw new RuntimeException();
+		}
+		// expression =infixToPostfix(expression);
+
+		for (int i = 0; i < expression.length(); i++) {
+			if (('a' <= (expression.charAt(i)) && 'z' >= (expression.charAt(i)))
+					|| ('A' <= (expression.charAt(i)) && 'Z' >= (expression.charAt(i)))) {
+
+				throw new RuntimeException("lolololol");
+			}
+
+		}
+
+		for (int i = 0; i < expression.length(); i++) {
+			if (expression.charAt(i) == ' ')
+				continue;
+			if (expression.charAt(i) != '('
+					// && expression.charAt(i) != ' '
+					&& expression.charAt(i) != '*' && expression.charAt(i) != '+' && expression.charAt(i) != '-'
+					&& expression.charAt(i) != '/' && expression.charAt(i) != ')') {
 				stack.push((double) (char) Character.getNumericValue(expression.charAt(i)));
 			} else if (expression.charAt(i) == '*') {
 				if (stack.isEmpty())
@@ -251,26 +227,25 @@ package eg.edu.alexu.csd.datastructure.stack.cs31;
 					throw new RuntimeException();
 				else if (operand1 == 0)
 					stack.push(0);
-					else stack.push((double)operand1/operand2);
-				}
-				
+				else
+					stack.push((double) operand1 / operand2);
 			}
-			if (stack.isEmpty())
-				 throw new RuntimeException();
-			 
-				 
-			return (int)(double)stack.pop();
-			 
-		
-			
+
 		}
-public static void main(String[]args){
-	IExpressionEvaluator app=new ExpressionEvaluator();
-	String str="( 9+10)";
-	String sol= app.infixToPostfix(str);
-	int ans=app.evaluate(sol);
-	System.out.println(sol);
-	
-	System.out.println(ans);
-}
+		if (stack.isEmpty())
+			throw new RuntimeException();
+
+		return (int) (double) stack.pop();
+
 	}
+
+	public static void main(String[] args) {
+		IExpressionEvaluator app = new ExpressionEvaluator();
+		String str = " 9 +10 /5+32+6/8";
+		String sol = app.infixToPostfix(str);
+		// int ans=app.evaluate(sol);
+		System.out.println(sol);
+
+		// System.out.println(ans);
+	}
+}
