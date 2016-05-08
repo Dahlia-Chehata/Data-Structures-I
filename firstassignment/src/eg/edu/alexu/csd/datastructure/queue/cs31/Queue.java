9bf3d2ba7ea7091fc9da34147bab61237b8bf4aa
Dell <Dell@Dell-PC>
2016-05-08 02:58:11 +0200

    1

---
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/queue/cs31/Queue.java
@@ -0,0 +1,35 @@
+package eg.edu.alexu.csd.datastructure.queue.cs31;
+import eg.edu.alexu.csd.datastructure.queue.IQueue;
+import eg.edu.alexu.csd.datastructure.queue.ILinkedBased;
+import eg.edu.alexu.csd.datastructure.linkedList.ILinkedList;
+import eg.edu.alexu.csd.datastructure.linkedList.cs31.*;
+
+public class Queue implements IQueue,ILinkedBased {
+ ILinkedList mylist =new doubly_linkedlists();
+	@Override
+	public void enqueue(Object item) {
+		// TODO Auto-generated method stub
+		mylist.add(item);
+	}
+
+	@Override
+	public Object dequeue() {
+		// TODO Auto-generated method stub
+		Object item = mylist.get(0);
+		mylist.remove(0);
+		return item;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return mylist.isEmpty();
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return mylist.size();
+	}
+
+}
---
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/queue/cs31/QueueArraybased.java
@@ -0,0 +1,31 @@
+package eg.edu.alexu.csd.datastructure.queue.cs31;
+import eg.edu.alexu.csd.datastructure.queue.IQueue;
+import eg.edu.alexu.csd.datastructure.queue.IArrayBased;
+
+public class QueueArraybased implements IQueue,IArrayBased {
+
+	@Override
+	public void enqueue(Object item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
