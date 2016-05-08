371a7b89d66b21e51db8b7f6ac177c29f8fe9782
Dell <Dell@Dell-PC>
2016-05-08 04:16:56 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/queue/cs31/QueueArraybased.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/queue/cs31/QueueArraybased.java
@@ -1,42 +1,46 @@
 package eg.edu.alexu.csd.datastructure.queue.cs31;
-import eg.edu.alexu.csd.datastructure.queue.IQueue;
-import eg.edu.alexu.csd.datastructure.queue.*;
 
-public class QueueArraybased implements IQueue,IArrayBased {
-   Object[] arr;
-   int size;
-   int n ,r,f;
-   public QueueArraybased(Object[] arr,int n){
-	   this.arr=arr;
-	   this.n=n;
-	   arr= new Object[n];
-	   size=0;
-	   r=0;
-	   f=0;
-   }
-	@Override
-	public void enqueue(Object item) throws RuntimeException {
-		// TODO Auto-generated method stub
-		if (size>=n)
-			throw new RuntimeException();
-		arr[r]=item;
-		if(r==n-1)
-			r=0 ;
-		else r++;
-			size++;
+import eg.edu.alexu.csd.datastructure.queue.IQueue;
+import eg.edu.alexu.csd.datastructure.queue.IArrayBased;
+
+public class QueueArraybased implements IQueue {
+	Object[] arr;
+	int size;
+	int n, r, f;
+
+	public QueueArraybased(Object[] arr, int n) {
+		this.arr = arr;
+		this.n = n;
+		arr = new Object[n];
+		size = 0;
+		r = 0;
+		f = 0;
 	}
 
 	@Override
-	public Object dequeue() throws RuntimeException  {
+	public void enqueue(Object item) throws RuntimeException {
 		// TODO Auto-generated method stub
-		if(size==0)
+		if (size >= n)
 			throw new RuntimeException();
-		Object temp= arr[f];
-		arr[f]=null;
-		if (f==n-1)
-			f=0;
-			else
-		f++;
+		arr[r] = item;
+		if (r == n - 1)
+			r = 0;
+		else
+			r++;
+		size++;
+	}
+
+	@Override
+	public Object dequeue() throws RuntimeException {
+		// TODO Auto-generated method stub
+		if (size == 0)
+			throw new RuntimeException();
+		Object temp = arr[f];
+		arr[f] = null;
+		if (f == n - 1)
+			f = 0;
+		else
+			f++;
 		size--;
 		return temp;
 	}
@@ -44,7 +48,7 @@ public class QueueArraybased implements IQueue,IArrayBased {
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return (size==0);
+		return (size == 0);
 	}
 
 	@Override
