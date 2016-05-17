9ce5ef45609742bbc249a27d85421fd9bdc10a66
Dell <Dell@Dell-PC>
2016-05-17 19:49:02 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/maze/cs31/Maze.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/maze/cs31/Maze.java
@@ -16,7 +16,7 @@ public class Maze implements IMazeSolver  {
 		char[][]input= appl.readFile(maze);
 		BfsSolver app=new BfsSolver();
 		int[][] arr=app.bfs(input,readfile.n,readfile.m);
-		if (arr.length==0||arr==null)
+		if (arr.length==0)
 				return null;
 		return arr;
 		
