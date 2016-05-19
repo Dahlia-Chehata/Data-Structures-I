8368cf9bb79f6b731eeee4e255a7b06c28312716
Dell <Dell@Dell-PC>
2016-05-19 19:03:50 +0200

    1

--- firstassignment/src/eg/edu/alexu/csd/datastructure/queue/cs31/QueueArraytest.java
+++ firstassignment/src/eg/edu/alexu/csd/datastructure/queue/cs31/QueueArraytest.java
@@ -6,32 +6,25 @@ import org.junit.Test;
  */
 public class QueueArraytest {
 /**..
- *. 
  */
 QueueArraybased list2;
 	/**..
-	 * .
 	 */
 private final int num5 = 5;
 	/**..
-	 * .
 	 */
 private final int num3 = 3;
 	/**.
-	 * .
 	 */
 private final int num8 = 8;
 	/**.
-	 * .
 	 */
 private final int num12 = 12;
 	/**.
-	 * .
 	 */
 private final int num10 = 10;
 	// enqueue item at index > size
 	/**.
-	 * ;
 	 */
 	@Test
 public void test1() {
